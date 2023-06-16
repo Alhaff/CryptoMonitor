b79cd2fdb33c6aaeadeefb4c3687e40a86608842
alhaffthemage@gmail.com <alhaffthemage@gmail.com>
2023-06-16 10:57:45 +0300

    Added Hosting suport for the future services, changed App file to suport hosting

--- CryptoMonitor/App.xaml.cs
+++ CryptoMonitor/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -8,10 +11,44 @@ using System.Windows;
 
 namespace CryptoMonitor
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private readonly IHost host;
+        public App()
+        {
+            host = Host.CreateDefaultBuilder()
+                   .ConfigureServices((context, services) =>
+                   {
+                       ConfigureServices(context.Configuration, services);
+                   })
+                   .Build();
+        }
+
+        private void ConfigureServices(IConfiguration configuration,
+            IServiceCollection services)
+        {
+            // ...
+            services.AddSingleton<MainWindow>();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await host.StartAsync();
+
+            var mainWindow = host.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using (host)
+            {
+                await host.StopAsync(TimeSpan.FromSeconds(5));
+            }
+
+            base.OnExit(e);
+        }
     }
 }
