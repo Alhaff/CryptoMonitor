523af5a5fcff03a84995843c7681e5805eb4a18c
alhaffthemage@gmail.com <alhaffthemage@gmail.com>
2023-06-17 17:54:06 +0300

    Added theme setings to main view model

--- CryptoMonitor/ViewModels/MainWindowViewModel.cs
+++ CryptoMonitor/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,25 @@
-﻿using CryptoMonitor.Services;
+﻿using CryptoMonitor.Core;
+using CryptoMonitor.Services;
+using Microsoft.Windows.Themes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+using Wpf.Ui.Appearance;
 using Wpf.Ui.Controls.Interfaces;
 using Wpf.Ui.Controls.Navigation;
 using Wpf.Ui.Mvvm.Contracts;
+using Wpf.Ui.Mvvm.Services;
 
 namespace CryptoMonitor.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
         private Services.INavigationService _navigation;
-        public Services.INavigationService Navigation 
+        public Services.INavigationService Navigation
         {
             get => _navigation;
             set
@@ -23,13 +29,41 @@ namespace CryptoMonitor.ViewModels
             }
         }
         private IThemeService _themeService;
-        public IThemeService ThemeService 
-        { 
+        public IThemeService ThemeService
+        {
             get => _themeService;
             set
             {
                 _themeService = value;
                 OnPropertyChanged(nameof(ThemeService));
+            }
+        }
+
+        private string iconPath = "pack://application:,,,/Resources/appIconWhite.ico";
+        public string IconPath
+        {
+            get
+            {
+                return iconPath;
+            }
+            set
+            {
+                iconPath = value;
+                OnPropertyChanged(nameof(IconPath));
+            }
+        }
+
+        private SolidColorBrush themeColor = new SolidColorBrush(Colors.Black);
+        public SolidColorBrush ThemeColor
+        {
+            get
+            {
+                return themeColor;
+            }
+            set
+            {
+                themeColor = value;
+                OnPropertyChanged(nameof(ThemeColor));
             }
         }
 
@@ -39,5 +73,40 @@ namespace CryptoMonitor.ViewModels
             ThemeService = themeService;
             Navigation.NavigateTo<Top10CoinViewModel>();
         }
+
+        private void LightTheme(object e)
+        {
+            ThemeService.SetTheme(ThemeType.Light);
+            ThemeColor = new SolidColorBrush(Colors.White);
+            IconPath = "pack://application:,,,/Resources/appIconBlack.ico";
+            
+        }
+
+        private void DarkTheme(object e)
+        {
+            ThemeService.SetTheme(ThemeType.Dark);
+            ThemeColor = new SolidColorBrush(Colors.Black);
+            IconPath = "pack://application:,,,/Resources/appIconWhite.ico";
+        }
+
+        private RelayCommand applyLightTheme;
+        public RelayCommand ApplyLightTheme
+        {
+            get
+            {
+                return applyLightTheme ?? (applyLightTheme = new RelayCommand(LightTheme));
+            }
+        }
+
+        private RelayCommand applyDarkTheme;
+
+        public RelayCommand ApplyDarkTheme
+        {
+            get
+            {
+                return applyDarkTheme ?? (applyDarkTheme = new RelayCommand(DarkTheme));
+            }
+        }
+
     }
 }
