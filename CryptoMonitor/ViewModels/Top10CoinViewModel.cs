b798b7ea8bfdb020a4a79266b2c430a5555a9517
alhaffthemage@gmail.com <alhaffthemage@gmail.com>
2023-06-17 15:26:49 +0300

    Added test Top10Coin viewmodel

---
+++ CryptoMonitor/ViewModels/Top10CoinViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoMonitor.ViewModels
+{
+    public class Top10CoinViewModel : ViewModelBase
+    {
+        public Top10CoinViewModel()
+        {
+            
+        }
+    }
+}
