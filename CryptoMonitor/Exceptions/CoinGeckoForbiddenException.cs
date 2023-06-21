2cc8c72191fac7e955852c0483fabefe1e93393a
alhaffthemage@gmail.com <alhaffthemage@gmail.com>
2023-06-21 23:11:50 +0300

    Updated functional of CoinGecko api service and created new folder for exceptions

---
+++ CryptoMonitor/Exceptions/CoinGeckoForbiddenException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CryptoMonitor.Exceptions
+{
+    [Serializable]
+    public class CoinGeckoForbiddenException : Exception
+    {
+        public CoinGeckoForbiddenException()
+        {
+        }
+
+        public CoinGeckoForbiddenException(string? message) : base(message)
+        {
+        }
+
+        public CoinGeckoForbiddenException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected CoinGeckoForbiddenException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ CryptoMonitor/Exceptions/UnsupportedCoinException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CryptoMonitor.Exceptions
+{
+    [Serializable]
+    public class UnsupportedCoinException : Exception
+    {
+        public UnsupportedCoinException()
+        {
+        }
+
+        public UnsupportedCoinException(string? message) : base(message)
+        {
+        }
+
+        public UnsupportedCoinException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected UnsupportedCoinException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
