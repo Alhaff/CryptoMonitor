d841eb00f635cf0f9b3c8bd9a26bdf7f8021312f
alhaffthemage@gmail.com <alhaffthemage@gmail.com>
2023-06-21 23:16:57 +0300

    Added service to control scroll to give opportunity to get access to main page scroll viewer for normal work with internal data grid scroll viewer on other pages.

---
+++ CryptoMonitor/Services/ScrollService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CryptoMonitor.Services
+{
+    public class ScrollService
+    {
+        public ScrollViewer ScrollViewer { get; set; }
+
+        public ScrollService()
+        {
+            ScrollViewer = new ScrollViewer();
+            ScrollViewer.VerticalScrollBarVisibility = ScrollBarVisibility.Visible;
+            ScrollViewer.HorizontalScrollBarVisibility = ScrollBarVisibility.Disabled;
+        }
+    }
+}
