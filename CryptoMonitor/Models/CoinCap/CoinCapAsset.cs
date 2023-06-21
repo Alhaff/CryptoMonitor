74e96be0ea1feee593db4ba5c9a1996234a0ec03
alhaffthemage@gmail.com <alhaffthemage@gmail.com>
2023-06-21 23:07:05 +0300

    Added Coin cap Api service and coinCap asset model, because responce to coin gecko api to markets branch was denied with code 1020. I can't solve this problem so I've added a coinCap api to get coins rank

---
+++ CryptoMonitor/Models/CoinCap/CoinCapAsset.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoMonitor.Models.CoinCap
+{
+    public class CoinCapAsset
+    {
+        public string Id { get; set; }
+        public int Rank { get; set; }
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public double? VolumeUsd24Hr { get; set; }
+        public double? MarketCapUsd { get; set; }
+        public double? PriceUsd { get; set; }
+        public double? ChangePercent24Hr { get; set; }
+        public static double TotalMarketCapUsd { get; set; } = 0;
+        public double? MarketCapPercent { get => ((MarketCapUsd / TotalMarketCapUsd )* 100);}
+    }
+}
---
+++ CryptoMonitor/Services/CoinCapApiService.cs
@@ -0,0 +1,47 @@
+﻿using CryptoMonitor.Models.CoinCap;
+using CryptoMonitor.Models.CoinGecko;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoMonitor.Services
+{
+    public class CoinCapApiService
+    {
+        HttpClient HttpClient { get; init; }
+        public CoinCapApiService(IHttpClientFactory httpClientFactory)
+        {
+            HttpClient = httpClientFactory.CreateClient();
+        }
+
+        public int CoinsPerPage { get; } = 50;
+
+        public int CoinsOffset { get; set; } = 0;
+
+        public async  Task<List<CoinCapAsset>> GetCoinsAssets(int page = 1)
+        {
+            if(page >=1)
+            {
+                CoinsOffset = (page-1) * CoinsPerPage;
+            }
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, $"https://api.coincap.io/v2/assets?limit={CoinsPerPage}&offset={CoinsOffset}");
+                var response = await HttpClient.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+                var res = await response.Content.ReadAsStringAsync();
+                var obj = JObject.Parse(res);
+                var coins = obj["data"].ToObject<List<CoinCapAsset>>();
+                return coins;
+            }catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+    }
+}
