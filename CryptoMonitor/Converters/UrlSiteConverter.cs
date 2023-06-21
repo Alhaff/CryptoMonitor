ff38bca883aeb35602a41643413387826d211b47
alhaffthemage@gmail.com <alhaffthemage@gmail.com>
2023-06-21 23:10:20 +0300

    Added converter to get only domain name from site url

---
+++ CryptoMonitor/Converters/UrlSiteConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CryptoMonitor.Converters
+{
+    public class UrlSiteConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string)
+            {
+                var str = value as string;
+                Uri url = new Uri(str);
+                return url.Authority;
+            }
+
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
