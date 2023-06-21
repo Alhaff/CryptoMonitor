eddf5ccebe272f5910c66182a93dfa779d0c5410
alhaffthemage@gmail.com <alhaffthemage@gmail.com>
2023-06-21 23:01:37 +0300

    Added Coin basic data model which contans name. id, and symbol

---
+++ CryptoMonitor/Models/CoinGecko/CoinShortData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoMonitor.Models.CoinGecko
+{
+    public class CoinShortData
+    {
+        public string Id { get; set; }
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+
+        public string NameSymbol { get=> this.ToString(); }
+        public override string ToString()
+        {
+            return $"{Name} {Symbol.ToUpper()}"; ;
+        }
+    }
+}
